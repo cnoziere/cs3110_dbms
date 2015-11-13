8b1ca219d8bf78124298e5bed2234a99cddf8f70
Constance <cnoziere@yahoo.com>
2015-11-12 22:49:28 -0500

    readJson.ml, updateJson.ml: finished interfaces

--- readJson.mli
+++ readJson.mli
@@ -1,19 +1,21 @@
+(* This module handles the creation of databases and tables from files *)
 open Yojson
 
-(* create the database specified by the JSON file with name filename *)
+(* create the database specified by contents of the file with the given
+filename *)
 (* returns false if the file cannot be found or a database with that
 name already exists *)
-val read_JSON: (filename : string) -> bool
+val read_JSON: string -> bool
 
-(* convert the contents of filename to a JSON value *)
-val convert: (filename : string) -> Yojson.Basic.json
+(* convert the contents of the file with the given filename to a JSON value *)
+val file_to_JSON: string -> Yojson.Basic.json
 
-(* creates the database specified by the JSON value json *)
+(* creates the database specified by the JSON value *)
 (* returns true if creation of the database was successful *)
-(* otherwise returns false if the json file could not be parsed *)
-val create_database: (json : Yojson.Basic.json) -> bool
+(* otherwise returns false if the JSON value could not be parsed *)
+val create_database: Yojson.Basic.json -> bool
 
-(* creates and fills the table as specified by the JSON value json *)
+(* creates and fills the table as specified by the JSON value *)
 (* returns true if creation of the table was successful *)
-(* otherwise returns false if the json file could not be parsed *)
-val create_full_table (json : Yojson.Basic.json) -> bool
+(* otherwise returns false if the JSON value could not be parsed *)
+val create_full_table: Yojson.Basic.json -> bool
---
+++ updateJson.mli
@@ -0,0 +1,14 @@
+open InternalRep
+
+(* converts a table into a JSON value *)
+val table_to_JSON: table -> Yosjon.Basic.json
+
+(* converts a database into a JSON value *)
+val database_to_JSON: database -> Yosjon.Basic.json
+
+(* writes a JSON value to the specified file name *)
+val JSON_to_file: Yojson.Basic.json -> string -> unit
+
+(* checks to see if the database has been updated and if so
+writes the database to file *)
+val watch_for_update: database -> unit
