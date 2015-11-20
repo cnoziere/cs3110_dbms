4ab9911a9ef74a4221b29f358f41bfa1ed01e034
Constance <cnoziere@yahoo.com>
2015-11-19 20:39:37 -0500

    updateJSON.ml: implemented watch_for_update function

--- updateJson.ml
+++ updateJson.ml
@@ -13,7 +13,12 @@ let database_to_JSON d = failwith "TODO"
 
 (* writes a JSON value to the specified file name *)
 let JSON_to_file json = failwith "TODO"
+(* Yojson.Basic to_file string json*)
+
+let updated = Ivar.read
 
 (* checks to see if the database has been updated and if so
 writes the database to file *)
-let watch_for_update d = failwith "TODOD"
+let watch_for_update d =
+  upon (updated d) (fun d' -> watch_for_update d';
+    JSON_to_file (database_to_JSON d))
