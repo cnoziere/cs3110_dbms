66a67099b789fbbc3eef9784f7f74d2f75c2d8c8
Constance <cnoziere@yahoo.com>
2015-11-27 22:04:15 -0500

    readJson.ml: now column major form

--- updateJson.mli
+++ updateJson.mli
@@ -1,15 +1,12 @@
-open Types
-open Yojson.Basic
-
-(* converts a table into a JSON value *)
-val table_to_JSON: table -> Yosjon.Basic.json
-
-(* converts a database into a JSON value *)
-val database_to_JSON: database -> Yosjon.Basic.json
-
 (* writes a JSON value to the specified file name *)
-val JSON_to_file: Yojson.Basic.json -> string -> unit
+val json_to_file: Yojson.Basic.json -> string -> unit
 
 (* checks to see if the database has been updated and if so
 writes the database to file *)
-val watch_for_update: database -> unit
+val watch_for_update: Types.database -> unit
+
+(* converts a table into a JSON value *)
+val table_to_json: Types.table -> Yojson.Basic.json
+
+(* converts a database into a JSON value *)
+val database_to_json: Types.database -> Yojson.Basic.json
