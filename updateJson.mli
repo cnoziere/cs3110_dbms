cfce7bfe86ea4aafd4a8b5a874204e081449efd7
Constance <cnoziere@yahoo.com>
2015-11-28 09:15:52 -0500

    updateJson: fixes to parse_column

--- updateJson.mli
+++ updateJson.mli
@@ -1,12 +1,16 @@
+open Async.Std
+
+(* type database = {name:string; tables: table Tst.tree; updated : database Ivar.t} *)
+
 (* writes a JSON value to the specified file name *)
 val json_to_file: Yojson.Basic.json -> string -> unit
 
 (* checks to see if the database has been updated and if so
 writes the database to file *)
-val watch_for_update: Types.database -> unit
+val watch_for_update: InternalRep.database -> unit
 
 (* converts a table into a JSON value *)
-val table_to_json: Types.table -> Yojson.Basic.json
+val table_to_json: InternalRep.table -> Yojson.Basic.json
 
 (* converts a database into a JSON value *)
-val database_to_json: Types.database -> Yojson.Basic.json
+val database_to_json: InternalRep.database -> Yojson.Basic.json
