1cb934701eaeb82ad4556ceb8991bfa19b1d8022
yanastali <yanastali@gmail.com>
2015-11-29 10:38:54 -0500

    test_internalRep.ml: add tests

---
+++ test_internal_rep.ml
@@ -0,0 +1,124 @@
+open InternalRep
+(* open Async.Std *)
+open Types
+
+
+(*
+(* Test the deferred d is determined to be the value v *)
+let test_async_eq (d: 'a Deferred.t) (v: 'a): bool =
+    Thread_safe.block_on_async (fun () -> d) = Core.Std.Result.Ok v
+
+(* Test the deferred d is determined to be the value v within timespan t *)
+let test_async_eq_with_timeout (t: float) (d: 'a Deferred.t) (v: 'a): bool =
+    Thread_safe.block_on_async (fun ()
+    -> with_timeout (Core.Std.sec t) d) = Core.Std.Result.Ok (`Result v)
+
+(* Test the deferred d is not determined to be the value v within timespan t *)
+let test_async_timeout (t: float) (d: 'a Deferred.t): bool =
+    Thread_safe.block_on_async (fun ()
+    -> with_timeout (Core.Std.sec t) d) = Core.Std.Result.Ok `Timeout
+
+(* Test if the value at val_ref is as expected after time delay *)
+let check_val (val_ref: int ref) (expected: int) (delay: float): bool =
+    Thread_safe.block_on_async (fun () -> after (Core.Std.sec delay)
+    >>= fun () -> return !val_ref) = Core.Std.Result.Ok expected
+
+(* Test if the values at ref1 and ref2 are as expected after time delay *)
+let check_two_vals (ref1: int ref) (val1: int)
+        (ref2: int ref) (val2: int) (t: float): bool =
+    Thread_safe.block_on_async (fun () -> after (Core.Std.sec t) >>=
+    fun () -> return (!ref1, !ref2)) = Core.Std.Result.Ok (val1, val2)
+*)
+
+
+TEST "CREATE_TABLE_returns_success" =
+    print_endline "CREATE_TABLE_returns_success";
+    match create_table "test" ["a"; "b"; "c"] with
+    | Success -> true
+    | Failure msg -> print_endline msg; false
+    | _ -> false
+
+TEST "CREATE_TABLE_duplicate_table" =
+    print_endline "CREATE_TABLE_duplicate_table";
+    match create_table "test" ["d"; "e"; "f"] with
+    | Failure "Table name already exists in database" -> true
+    | _ -> false
+
+TEST "CREATE_TABLE_duplicate_col" =
+    print_endline "CREATE_TABLE_duplicate_col";
+    match create_table "test2" ["d"; "e"; "f"; "e"] with
+    | Failure "Duplicate column name used to initialize table" -> true
+    | _ -> false
+
+(*
+TEST_UNIT "UPDATED_not_determined" =
+    print_endline "UPDATED_not_determined";
+    upon (updated ()) (fun () -> print_endline "Print: UPDATED_not_determined")
+
+TEST "UPDATED_determined" =
+    print_endline "UPDATED_determined";
+    upon (updated ()) (fun () -> print_endline "Print: UPDATED_determined");
+    match create_table "a" ["a"; "b"; "c"] with
+    | Success -> true
+    | Failure msg -> print_endline msg; false
+    | _ -> false
+*)
+
+TEST "DROP_added_table" =
+    print_endline "DROP_added_table";
+    ignore (create_table "b" ["a"; "b"; "c"]);
+    match drop_table "b" with
+    | Success -> true
+    | Failure msg -> print_endline msg; false
+    | _ -> false
+
+TEST "DROP_table_add" =
+    print_endline "DROP_table_add";
+    ignore (create_table "b" ["a"; "b"; "c"]);
+    ignore (drop_table "b");
+    match create_table "b" ["a"; "b"; "c"] with
+    | Success -> true
+    | Failure msg -> print_endline msg; false
+    | _ -> false
+
+TEST "ADD_ROW_success" =
+    print_endline "ADD_ROW_success";
+    ignore (create_table "addrow" ["name"; "grade"; "age"]);
+    match add_row "addrow" ["name"; "grade"] ["asta"; "college"] with
+    | Success -> true
+    | Failure msg -> print_endline msg; false
+    | _ -> false
+
+TEST "ADD_ROW_partial_list" =
+    print_endline "ADD_ROW_partial_list";
+    match add_row "addrow" ["grade"] ["college"] with
+    | Success -> true
+    | Failure msg -> print_endline msg; false
+    | _ -> false
+
+TEST "ADD_ROW_no_table_failure" =
+    print_endline "ADD_ROW_no_table_failure";
+    match add_row "notable" ["grade"] ["college"] with
+    | Failure _ -> true
+    | _ -> false
+
+TEST "ADD_ROW_failure" =
+    print_endline "ADD_ROW_failure";
+    match add_row "addrow" ["year"] ["college"] with
+    | Failure _ -> true
+    | _ -> false
+
+TEST "DELETE_ROW_success" =
+    print_endline "DELETE_ROW_success";
+    match delete_row "addrow" 1 with
+    | Success -> true
+    | Failure msg -> print_endline msg; false
+    | _ -> false
+
+TEST "DELETE_ROW_failure" =
+    print_endline "DELETE_ROW_failure";
+    match delete_row "addrow" 5 with
+    | Failure "Key does not exist" -> true
+    | _ -> false
+
+(* let _ = Scheduler.go () *)
