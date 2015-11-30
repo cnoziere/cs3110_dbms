ccd0ecb6f651c1bb612bffbe65b78abd3f37e465
Constance <cnoziere@yahoo.com>
2015-11-29 22:16:12 -0500

    updateJson.ml: implemented database_to_file

--- updateJson.ml
+++ updateJson.ml
@@ -8,7 +8,7 @@ let to_string (lst : string list) =
 
 (* converts a table into a JSON value *)
 let table_to_json (tablename : string) =
-  let err_string = ("Cannot convert " ^ tablename ^ " to json format") in
+  let err_string = ("Cannot convert table " ^ tablename ^ " to json format") in
   match (get_column_names tablename) with
   | ColNames column_names ->
       let alwaystrue = fun x -> true in
@@ -26,8 +26,8 @@ let table_to_json (tablename : string) =
       else let f' = fun c -> match c with
                              | Column v -> `List (to_string v)
                              | _ -> failwith "This will never pattern match" in
-      let columns' = List.map f' columns in
 
+      let columns' = List.map f' columns in
       let table = `Assoc [("tableName", `String tablename);
       ("columnNames", `List (to_string column_names));
       ("columns", `List columns')] in
@@ -36,19 +36,32 @@ let table_to_json (tablename : string) =
   | _ -> Failure err_string
 
 (* converts a database into a JSON value *)
-let database_to_json (database : string) =
+let database_to_json (dbname : string) =
   failwith "TODO"
-  (* let r = ref [] in
-  traverse_database "" r database;
-  `Assoc [("dbName", `String databasename);
-  ("tables", `List !r)] *)
+  (* let err_string = ("Cannot convert database " ^ dbname ^ " to json format") in
+  let tablenames = ... in
+  let result = Ivar.create () in
+  let f = fun t -> (let table = table_to_json t in
+                    let () = (match table with
+                              | Json _ -> ()
+                              | _ -> if Ivar.is_empty result
+                                     then Ivar.fill result ()
+                                     else ()) in
+                    table) in
+
+    let tables = List.map f tablenames in
+    if Ivar.is_full result then Failure err_string
+    else let db = `Assoc [("dbName", `String dbname); ("tables", `List tables)] in
+    Json db *)
 
 (* writes a JSON value to the specified file name *)
-let json_to_file json = failwith "TODO"
-(* Yojson.Basic to_file string json*)
+let database_to_file (dbname : string) =
+  match database_to_json dbname with
+  | Json db -> Yojson.Basic.to_file dbname db; Success
+  | _ -> Failure ("Cannot write database " ^ dbname ^ " to file")
 
 (* checks to see if the database has been updated and if so
 writes the database to file *)
 let watch_for_update d = failwith "TODO"
   (* upon (Ivar.read d) (fun d' -> watch_for_update d';
-    json_to_file (database_to_json d)) *)
+    (database_to_file d)) *)
--- updateJson.mli
+++ updateJson.mli
@@ -3,15 +3,15 @@ open InternalRep
 
 (* type database = {name:string; tables: table Tst.tree; updated : database Ivar.t} *)
 
-(* writes a JSON value to the specified file name *)
-val json_to_file: Yojson.Basic.json -> string -> unit
-
-(* checks to see if the database has been updated and if so
-writes the database to file *)
-val watch_for_update: string -> unit
-
 (* converts a table into a JSON value *)
 val table_to_json: string -> Types.result
 
 (* converts a database into a JSON value *)
 val database_to_json: string -> Yojson.Basic.json
+
+(* writes a JSON value to the specified file name *)
+val database_to_file: string -> Types.result
+
+(* checks to see if the database has been updated and if so
+writes the database to file *)
+val watch_for_update: string -> unit
