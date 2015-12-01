ba6422ea7235c09fc5861414fda9aad301a712ae
Constance <cnoziere@yahoo.com>
2015-12-01 12:32:01 -0500

    updateJson.ml: implemented writing to file at the table level

--- updateJson.mli
+++ updateJson.mli
@@ -1,17 +1,30 @@
 open Async.Std
 open InternalRep
 
-(* type database = {name:string; tables: table Tst.tree; updated : database Ivar.t} *)
-
-(* converts a table into a JSON value *)
+(* [table_to_json t] converts the table with table name t of the current
+ * database into a JSON value.
+ * Returns Success if the operation was successfully completed
+ * Otherwise returns Failure some_error_message *)
 val table_to_json: string -> Types.result
 
-(* converts a database into a JSON value *)
-val database_to_json: unit -> Types.result
+(* [table_to_file t] writes the JSON value that represents the table t
+ * of the current database to file.
+ * Writing to file is done asynchronously so a deferred is returned.
+ * The deferred becomes determine with value Success if the operation
+ * was successfully completed. Otherwise it becomes determined with
+ * Failure some_error_message.
+ * The file is written to the file 'dbname_tablename.json' where dbname
+ * is the name of the database and tablename is the name of the table. *)
+val table_to_file: string -> Types.result Deferred.t
 
-(* writes a JSON value to the specified file name *)
-val database_to_file: unit -> Types.result Deferred.t
+(* [databse_to_json ()] converts the current database into a JSON value *)
+val database_to_json: unit -> Yojson.Basic.json
 
-(* checks to see if the database has been updated and if so
-writes the database to file *)
+(* [database_to_file ()] writes the current database to file.
+ * The database is written to the file 'dbname.json' where dbname
+ * is the name of the database. *)
+val database_to_file: unit -> unit
+
+(* [watch_for_update ()] writes a table to file when it has been modified
+ * or writes the current database to file when a table is added/removed. *)
 val watch_for_update: unit -> unit
