e9c07111a972ee0c2fff17993db09e51092010ec
Constance <cnoziere@yahoo.com>
2015-12-02 17:01:38 -0500

    readJson: added try/catch blocks and error messages

--- readJson.ml
+++ readJson.ml
@@ -3,15 +3,17 @@ open Types
 open Operation
 open Yojson.Basic
 
-let rec one_by_one f = function
-  | [] -> Success
+let rec one_by_one (db : database) f = function
+  | [] -> Success db
   | hd :: tl -> (match f hd with
-                | Success -> one_by_one f tl
+                | Success _ -> one_by_one db f tl
                 | Failure s -> Failure s
                 | _ -> failwith "This should never pattern match")
 
 let ok_to_create_database (dbname : string) =
-    not (Sys.file_exists dbname)
+    let res = not (Sys.file_exists dbname) in
+    if res then Unix.mkdir dbname 0o666 else ();
+    res
 
 let to_column jsoncol =
     let open Yojson.Basic.Util in
@@ -19,68 +21,88 @@ let to_column jsoncol =
     |> to_list
     |> List.map to_string
 
-let create_full_table json =
+let create_full_table (db: database) (tablename : string) json =
     let open Yojson.Basic.Util in
-    let table_name =                    (* table_name : string *)
-        json
-        |> member "tableName"
-        |> to_string in
+    match
+      (try
+        let table_name =                    (* table_name : string *)
+          json
+          |> member "tableName"
+          |> to_string in
 
-    let column_names =                  (* column_names : string list *)
-        [json]
-        |> filter_member "columnNames"
-        |> flatten
-        |> List.map to_string in
+        let column_names =                  (* column_names : string list *)
+          [json]
+          |> filter_member "columnNames"
+          |> flatten
+          |> List.map to_string in
 
-    let columns =                       (* columns : string list list *)
-        [json]
-        |> filter_member "columns"
-        |> flatten
-        |> List.map to_column in
+        let columns =                       (* columns : string list list *)
+          [json]
+          |> filter_member "columns"
+          |> flatten
+          |> List.map to_column in
 
-    let rows =                          (* rows: string list list *)
-        match columns with
-        | [] -> []
-        | hd :: tl -> let acc = List.map (fun a -> [a]) hd in
-                      let f = List.map2 (fun a b -> a @ [b]) in
-                      List.fold_left f acc tl in
+        Some (table_name, column_names, columns)
+      with
+      | _ -> None)
+    with
+    | None -> Failure ("Cannot parse the json file for table " ^ tablename ^ "\n")
+    | Some (table_name, column_names, columns) ->
+      if List.length columns <> List.length column_names
+      then Failure ("Incorrectly formated json file for table " ^ tablename ^
+      ". columnNames and column should be of the same length.\n")
+      else InternalRep.create_whole_table db table_name column_names columns
 
-    let f = add_row table_name column_names in
+let load_table (db : database) (dbname: string) (tablename : string) =
+    let path = "./" ^ dbname ^ "/" ^ tablename ^ ".json" in
+    match
+      (try
+        Some (Yojson.Basic.from_file path)
+       with
+       | _ -> None)
+    with
+    | Some table -> create_full_table db tablename table
+    | None -> Failure ("Cannot find/parse table " ^ tablename ^ ".\n")
 
-    match create_table table_name column_names with
-    | Failure s -> Failure s
-    | Success -> one_by_one f rows
-    | _ -> failwith "This should never pattern match"
-
-let make_table (dbname: string) (tablename : string) =
-    let table = Yojson.Basic.from_file (dbname ^ "_" ^ tablename ^ ".json") in
-    create_full_table table
-
-let create_database json =
+let create_database (dbname: string) json =
     let open Yojson.Basic.Util in
-    let dbname =
-        json
-        |> member "dbName"
-        |> to_string in
-
-    let tables =
-        [json]
-        |> filter_member "tables"
-        |> flatten
-        |> List.map to_string in
-
-    InternalRep.set_name dbname;
-    (* maybe initialize a new databse? make sure the current database is empty *)
-    one_by_one (make_table dbname) tables
+    match
+      (try
+        let dbname =
+          json
+          |> member "dbName"
+          |> to_string in
+        let tables =
+          [json]
+          |> filter_member "tables"
+          |> flatten
+          |> List.map to_string in
+        Some (dbname, tables)
+      with
+      | _ -> None)
+    with
+    | None -> Failure ("Cannot parse database " ^ dbname ^ ".\n")
+    | Some (dbname, tables) -> (match InternalRep.create_database dbname with
+                                | Success db -> one_by_one db (load_table db dbname) tables
+                                | Failure s -> Failure s
+                                | _ -> failwith "This should never pattern match")
 
 (* filename should be of form __.json *)
-let read_db (filename : string) =
-  match
-    try Some (Yojson.Basic.from_file filename)
-    with _ -> None
-  with
-  | None -> Failure ("No such file in directory: " ^ filename)
-  | Some json -> create_database json
+let load_db (dbname : string) =
+    let path = "./" ^ dbname ^ "/" ^ dbname ^ ".json" in
+    match
+      (try
+      Some (Yojson.Basic.from_file path)
+      with _ -> None)
+    with
+    | None -> Failure ("Cannot find/parse the file at the following path: " ^ path ^ "\n")
+    | Some json -> create_database dbname json
 
 (* CHECK ALL FAILWITH NOOOOO *)
 (* TO COMPILE: cs3110 compile -t -p async -p yojson readJson.ml *)
+
+(* dropping tables ? *)
+
+(* changed read_db to load_db *)
+
+(* wait for asta's implementation in create_full_table *)
--- updateJson.ml
+++ updateJson.ml
@@ -6,13 +6,14 @@ let to_string (lst : string list) =
   List.map (fun x -> `String x) lst
 
 (* converts a table into a JSON value *)
-let table_to_json (tablename : string) =
+let table_to_json (db : database) (tablename : string) =
   let open Async.Std in
-  match (get_column_names tablename) with
+  let dbname = db.name in
+  match (get_column_names db tablename) with
   | ColNames column_names ->
       let alwaystrue = fun x -> true in
       let result = Ivar.create () in
-      let f = fun c -> get_column_vals tablename c alwaystrue in
+      let f = fun c -> get_column_vals db tablename c alwaystrue in
       let columns = List.map f column_names in
       let f' = fun c -> match c with
                         | Column v -> `List (to_string v)
@@ -21,54 +22,36 @@ let table_to_json (tablename : string) =
                                else (); `Null in
       let columns' = List.map f' columns in
       if Ivar.is_full result then
-      Failure ("Cannot convert table " ^ tablename ^ " to json format")
+      Failure ("Cannot convert table " ^ tablename ^ "from database " ^ dbname ^
+        " to json format")
       else let table = `Assoc [("tableName", `String tablename);
                        ("columnNames", `List (to_string column_names));
                        ("columns", `List columns')] in
       Json table
-  | _ -> Failure ("No table named " ^ tablename ^ " in current database")
+  | _ -> Failure ("No table named " ^ tablename ^ " in database " ^ dbname)
 
 (* writes a table JSON value to the specified file name *)
 (* attention: Writer.write overwrites the contents of the previous file *)
-let table_to_file (tablename : string) =
+let table_to_file (db : database) (tablename : string) =
   let open Async.Std in
-  let dbname = get_name () in
-  let name = dbname ^ "_" ^ tablename ^ ".json" in
+  let path =  "./" ^ dbname ^ "/" ^ tablename ^ ".json" in
   match table_to_json tablename with
   | Json table -> Writer.open_file "tmp.json" >>=
                   (fun t -> Writer.write t (pretty_to_string table);
                   Writer.close t >>=
-                  (fun () -> let _ = Sys.rename "tmp.json" name in
-                  return Success))
-  | _ -> let err_string = "Cannot write table " ^ tablename ^
-                          " from database " ^ dbname ^ " to file" in
-         return (Failure err_string)
+                  (fun () -> let _ = Sys.rename "tmp.json" path in
+                  return Success db))
+  | f -> return f
 
 (* converts a database into a JSON value *)
-let database_to_json () =
-  let dbname = get_name () in
-  let tablenames = get_table_names () in
-  `Assoc [("dbName", `String dbname);
+let database_to_json (db : database) =
+  let tablenames = get_table_names db in
+  `Assoc [("dbName", `String db.name);
   ("tables", `List (to_string tablenames))]
-
-  (* let err_string = ("Cannot convert database " ^ dbname ^ " to json format") in
-  let tablenames = get_table_names () in
-  let result = Ivar.create () in
-  let tables = List.map table_to_json tablenames in
-  let f' = fun t -> match t with
-                    | Json v -> v
-                    | _ -> if Ivar.is_empty result
-                           then Ivar.fill result ()
-                           else (); `Null in
-  let tables' = List.map f' tables in
-  if Ivar.is_full result then Failure err_string
-  else let db = `Assoc [("dbName", `String dbname);
-                ("tables", `List tables')] in
-  Json db *)
 
 (* writes a JSON value to the specified file name *)
 (* attention: Writer.write overwrites the contents of the previous file *)
-let database_to_file () =
+let database_to_file (db : database) =
   let db = database_to_json () in
   let dbname = get_name () in
   let d1 = let open Async.Std in
@@ -79,15 +62,6 @@ let database_to_file () =
     if Sys.file_exists name then Sys.remove name else ();
     Sys.rename "tmp.json" name)
 
-  (* let dbname = get_name () in
-  match database_to_json () with
-  | Json db -> Writer.open_file "tmp.json" >>=
-               (fun t -> Writer.write t (Yojson.Basic.to_string db);
-               Writer.close t >>=
-               (fun () -> let _ = Sys.rename "tmp.json" (dbname ^ ".json") in
-               return Success))
-  | _ -> return (Failure ("Cannot write database " ^ dbname ^ " to file")) *)
-
 (* checks to see if the database has been updated and if so
 writes the database to file *)
 let rec watch_for_update () =
@@ -97,6 +71,7 @@ let rec watch_for_update () =
 
 
 (*
+* drop table needed
 * Call table_to_file everytime a pre-existing table is modified.
 * Call databse_to_file everytime a new table is created.
 *)
