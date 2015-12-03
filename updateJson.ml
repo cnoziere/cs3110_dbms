daad2e1ef9665a88eccb0f893be73a8670b0f864
Constance <cnoziere@yahoo.com>
2015-12-03 13:34:52 -0500

    updatedJson: finished writing function specifications

--- updateJson.ml
+++ updateJson.ml
@@ -1,51 +1,42 @@
 open Yojson.Basic
 open InternalRep
-open Types
 open Async.Std
+open Types
 
 let to_string (lst : string list) =
   List.map (fun x -> `String x) lst
 
 let table_to_json (db : database) (tablename : string) =
-  let open Async.Std in
-  let dbname = db.name in
   match (get_column_names db tablename) with
   | ColNames column_names ->
       let alwaystrue = fun x -> true in
-      let result = Ivar.create () in
       let f = fun c -> get_column_vals db tablename c alwaystrue in
       let columns = List.map f column_names in
       let f' = fun c -> match c with
                         | Column v -> `List (to_string v)
-                        | _ -> if Ivar.is_empty result
-                               then Ivar.fill result ()
-                               else (); `Null in
+                        | _ -> failwith ("This should never pattern match") in
       let columns' = List.map f' columns in
-      if Ivar.is_full result then
-      Failure ("Cannot convert table " ^ tablename ^ "from database " ^ dbname ^
-        " to json format")
-      else let table = `Assoc [("tableName", `String tablename);
-                       ("columnNames", `List (to_string column_names));
-                       ("columns", `List columns')] in
-      Json table
+      let table = `Assoc [("tableName", `String tablename);
+                  ("columnNames", `List (to_string column_names));
+                  ("columns", `List columns')] in
+      table
   | _ -> failwith ("This should never pattern match")
 
-(* asynchronously writes the json value to the specified path
+(* Asynchronously writes the json value to the specified path.
+ * First backs data up to tmp.json and then overwrites path.
  * NB: Writer.write overwrites the contents of the file if it
  * previously existed *)
-let write_to_file json path =
-  let data = pretty_to_string json in
+let write_to_file (j : json) (path : string) =
+  let data = pretty_to_string j in
   Writer.open_file "tmp.json" >>=
   fun t -> Writer.write t data; Writer.close t >>=
   fun _ -> Writer.open_file path >>=
   fun t' -> Writer.write t' data; Writer.close t'
 
 let table_to_file (db : database) (tablename : string) =
-  let dbname = db.name in
-  let path =  "./" ^ dbname ^ "/" ^ tablename ^ ".json" in
-  match table_to_json db tablename with
-  | Json table -> write_to_file table path
-  | _ -> failwith ("This should never pattern match")
+  let path =  "./" ^ db.name ^ "/" ^ tablename ^ ".json" in
+  let json_table = table_to_json db tablename in
+  write_to_file json_table path
 
 let database_to_json (db : database) =
   let tablenames = get_table_names db in
@@ -53,23 +44,23 @@ let database_to_json (db : database) =
   ("tables", `List (to_string tablenames))]
 
 let database_to_file (db : database) =
-  let json = database_to_json db in
+  let json_db = database_to_json db in
   let dbname = db.name in
   let path  = "./" ^ dbname ^ "/" ^ dbname ^ ".json" in
-  write_to_file json path
+  write_to_file json_db path
 
-(* implements the observer pattern covered in Rec 18
- * whenever the database is updated, the deferred becomes determined
+(* Implements the observer pattern covered in Rec 18
+ * Whenever the database is updated, the deferred becomes determined
  * and the appropriate portion of the database is written to file *)
 let rec watch_for_update (db : database) =
   Async.Std.upon (updated db)
   (fun (db', tablename) -> watch_for_update db';
-    let check = fun x -> List.mem tablename (InternalRep.get_table_names x) in
+    let check = fun x -> List.mem tablename (get_table_names x) in
     let path = "./" ^ db.name ^ "/" ^ tablename ^ ".json" in
     match (check db, check db') with
-    | (true, true)   -> ignore (table_to_file db' tablename)  (* modified table *)
+    | (true, true)   -> ignore (table_to_file db' tablename)  (* table modified *)
     | (true, false)  -> ignore (Sys.remove path >>=
-                        fun () -> database_to_file db')       (* removed table *)
-    | (false, true)  -> ignore (table_to_file db' tablename >>= fun _ ->
-                        database_to_file db')                 (* added table *)
-    | (false, false) -> ())
+                        fun () -> database_to_file db')       (* table deleted *)
+    | (false, true)  -> ignore (table_to_file db' tablename >>=
+                        fun _ -> database_to_file db')        (* table added *)
+    | (false, false) -> ignore (database_to_file db'))
--- updateJson.mli
+++ updateJson.mli
@@ -1,30 +1,28 @@
+open Yojson.Basic
 open Async.Std
 open Types
 
 (* [table_to_json db t] converts the table named t of database db
- * into a JSON value.
- * Returns Success if the operation was successfully completed
- * Otherwise returns Failure some_error_message *)
-val table_to_json: database -> string -> Types.result
+ * into a JSON value. *)
+val table_to_json: database -> string -> json
 
-(* [table_to_file db t] writes the JSON value that represents the
+(* [databse_to_json db] converts db into a JSON value *)
+val database_to_json: database -> json
+
+(* [table_to_file db t] writes the JSON value that represents
  * table t of database db to file.
  * Writing to file is done asynchronously so a deferred is returned.
- * The deferred becomes determined with value Success if the operation
- * was successfully completed. Otherwise it becomes determined with
- * Failure some_error_message.
  * The file is written to the path './dbname/t.json' where dbname
  * is the name of db. *)
 val table_to_file: database -> string -> unit Deferred.t
 
-(* [databse_to_json db] converts db into a JSON value *)
-val database_to_json: database -> Yojson.Basic.json
-
-(* [database_to_file db] writes db file.
- * The database is written to the file './dbname/dbname.json' where
- * dbname is the name of db. *)
+(* [database_to_file db] writes database db file.
+ * The database is written to the path './dbname/dbname.json'
+ * where dbname is the name of db. *)
 val database_to_file: database -> unit Deferred.t
 
-(* [watch_for_update db] writes a table to file when it has been modified
- * or writes the current database to file when a table is added/removed. *)
+(* This function implements the observer pattern from Rec 18
+ * [watch_for_update db] performs one of the following:
+ * 1) if a table from db has been modified, it writes the new table to file
+ * 2) if a table has been added/removed, it writes the updated database to file *)
 val watch_for_update: database -> unit
