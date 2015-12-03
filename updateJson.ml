23235ae6bdf5dc5ff12337d73c5c7f9641987c4e
Constance <cnoziere@yahoo.com>
2015-12-03 00:51:50 -0500

    updateJson.ml: added comments

--- updateJson.ml
+++ updateJson.ml
@@ -6,7 +6,6 @@ open Async.Std
 let to_string (lst : string list) =
   List.map (fun x -> `String x) lst
 
-(* converts a table into a JSON value *)
 let table_to_json (db : database) (tablename : string) =
   let open Async.Std in
   let dbname = db.name in
@@ -31,8 +30,9 @@ let table_to_json (db : database) (tablename : string) =
       Json table
   | _ -> failwith ("This should never pattern match")
 
-(* writes a table JSON value to the specified file name *)
-(* attention: Writer.write overwrites the contents of the previous file *)
+(* asynchronously writes the json value to the specified path
+ * NB: Writer.write overwrites the contents of the file if it
+ * previously existed *)
 let write_to_file json path =
   let data = pretty_to_string json in
   Writer.open_file "tmp.json" >>=
@@ -47,30 +47,29 @@ let table_to_file (db : database) (tablename : string) =
   | Json table -> write_to_file table path
   | _ -> failwith ("This should never pattern match")
 
-(* converts a database into a JSON value *)
 let database_to_json (db : database) =
   let tablenames = get_table_names db in
   `Assoc [("dbName", `String db.name);
   ("tables", `List (to_string tablenames))]
 
-(* writes a JSON value to the specified file name *)
-(* attention: Writer.write overwrites the contents of the previous file *)
 let database_to_file (db : database) =
   let json = database_to_json db in
   let dbname = db.name in
   let path  = "./" ^ dbname ^ "/" ^ dbname ^ ".json" in
   write_to_file json path
 
-(* checks to see if the database has been updated and if so
-writes the database to file *)
+(* implements the observer pattern covered in Rec 18
+ * whenever the database is updated, the deferred becomes determined
+ * and the appropriate portion of the database is written to file *)
 let rec watch_for_update (db : database) =
   Async.Std.upon (updated db)
   (fun (db', tablename) -> watch_for_update db';
     let check = fun x -> List.mem tablename (InternalRep.get_table_names x) in
+    let path = "./" ^ db.name ^ "/" ^ tablename ^ ".json" in
     match (check db, check db') with
-    | (true, true)   -> ignore (table_to_file db' tablename)     (* modified table *)
-    | (true, false)  -> ignore (Sys.remove ("./" ^ db.name ^ "/" ^ tablename ^ ".json") >>=
-                        fun () -> database_to_file db')          (* removed table *)
+    | (true, true)   -> ignore (table_to_file db' tablename)  (* modified table *)
+    | (true, false)  -> ignore (Sys.remove path >>=
+                        fun () -> database_to_file db')       (* removed table *)
     | (false, true)  -> ignore (table_to_file db' tablename >>= fun _ ->
-                        database_to_file db')                    (* added table *)
+                        database_to_file db')                 (* added table *)
     | (false, false) -> ())
